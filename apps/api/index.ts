5dade8719e62f1b9ac459e7203d6c0161db831b7
akshat_1074 <valaniakshat1074@gmail.com>
2025-06-09 23:39:33 +0530

    add all code

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,20 +1,75 @@
 import express from "express"
+import  {authMiddleware } from "./middleware";
+import {prismaClient} from "db/client";
 
 const app =express();
+app.use(express.json());
 
-app.post("/api/v1/website",(req,res)=>{
+app.post("/api/v1/website",authMiddleware, async(req,res)=>{
+     const userId = req.userId;
+     const {url} = req.body;
+
+    await  prismaClient.website.create({
+            data:{
+               userId,
+               url
+          }
+     })
+
+     res.json({
+          id: data.id
+     })
+})
+
+app.get("/api/v1/website/status",authMiddleware,async (req,res)=>{
+    const websiteId = req.query.websiteId! as unknown as string;
+    const userId = req.userId;
+
+    const data = await prismaClient.website.findFirst({
+         where:{
+           id:websiteId,
+           userId
+         },
+         includes:{
+          ticks:true
+         }
+    })
+ 
+     res.json(data)
 
 })
 
-app.get("/api/v1/website/status",(req,res)=>{
-    
-})
+app.get("/api/v1/website",async(req,res)=>{
+    const userId = req.userId!;
 
-app.get("/api/v1/website",(req,res)=>{
-    
+    const websites = await prismaClient.website.findMany({
+     where:{
+          userId,
+          disabled:false
+     }
+    })
+    res.json({
+      websites
+    })
 })
 
 app.delete("/api/v1/website/",(req,res)=>{
-    
+    const websiteId = req.body.websiteId;
+    const userId = req.userId!;
+
+    await prismaClient.website.update({
+      where:{
+          id:websiteId,
+          userId
+      },
+      data:{
+          disabled:true
+      }
+    })
+
+    res.json({
+     message:"Deleted website successfully"
+    })
+
 })
-app.listen
+app.listen(3000);
